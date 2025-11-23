2c974e220de15dc25b3b55c3f51a48bf9044c6d5
Shabin Francis <shabinfrancis25@gmail.com>
2025-11-23 18:31:06 +0530

    Add GSAP library and update Home component for trip functionality

--- Frontend/src/pages/UserProtectWrapper.jsx
+++ Frontend/src/pages/UserProtectWrapper.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserDataContext } from '../context/UserContext'
 import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const UserProtectWrapper = ({ children }) => {
     const token = localStorage.getItem('token')
