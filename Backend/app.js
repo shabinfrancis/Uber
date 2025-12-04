ede257fc6c86c86430934591b4c247517a8f8fdf
Shabin Francis <shabinfrancis25@gmail.com>
2025-12-04 21:43:55 +0530

    Add ride routes and controller for ride management

--- Backend/app.js
+++ Backend/app.js
@@ -8,7 +8,7 @@ const connectToDb = require('./db/db')
 const userRoutes = require('./routes/user.route')
 const captainRoutes = require('./routes/captain.route');
 const mapRoutes = require('./routes/map.route');
-
+const rideRoutes = require('./routes/ride.route');
 connectToDb();
 
 app.use(cors());
@@ -23,5 +23,6 @@ app.get('/', (req, res) =>{
 app.use('/users', userRoutes);
 app.use('/captains', captainRoutes);
 app.use('/maps', mapRoutes);
+app.use('/rides', rideRoutes);
 
 module.exports = app;
---
+++ Backend/routes/ride.route.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middlewares/auth.middleware');
+const rideController = require('../controllers/ride.controller');
+const {query, body} = require('express-validator');
+
+router.post('/create',
+    authMiddleware.authUser,
+    body('pickup').isString().isLength({min: 3}.withMessage('Invalid pickup address')),
+    body('destination').isString().isLength({min: 3}).withMessage('Invalid destination address'),
+    body('vehicleType').isString().isLength({min: 3}).withMessage('Invalid vehicle type'),
+    
+)
+
+router.get('/get-fare',
+    authMiddleware.authUser,
+    query('pickup').isString().isLength({min: 3}.withMessage('Invalid pickup address')),
+    query('destination').isString().isLength({min: 3}).withMessage('Invalid destination address'),
+        
+)
+
+router.post('/confirm',
+    authMiddleware.authCaptain,
+    body('rideId').isMongoId().withMessage('Invalid ride id'),
+
+)
+
+router.get('start-ride',
+    authMiddleware.authCaptain,
+    query('rideId').isMongoId().withMessage('Invalid ride id'),
+    query('otp').isString().isLength({min: 6, max: 6}).withMessage('invalid OTP'),
+
+)
+
+router.post('/end-ride',
+    authMiddleware.authCaptain,
+    body('rideId').isMongoId().withMessage('Invalid ride id'),
+    
+)
+
+
+
+module.exports = router;
