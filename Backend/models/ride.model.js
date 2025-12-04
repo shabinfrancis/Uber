379a6d581b966949fb810bbb2dc01b31d0327f29
Shabin Francis <shabinfrancis25@gmail.com>
2025-12-04 21:43:04 +0530

    Create model for ride

---
+++ Backend/models/ride.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const rideSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    captain: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'captain',
+    },
+    pickup: {
+        type: String,
+        required: true,
+    },
+    destination: {
+        type: String,
+        required: true
+    },
+    fare: {
+        type: Number,
+        required: true
+    },
+    status: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number  //in seconds
+    },
+    distance: {
+        type: number  //in meters
+    },
+    paymentId: {
+        type: String
+    },
+    orderId: {
+        type: String
+    },
+    signature: {
+        type: String
+    },
+    otp: {
+        type: String,
+        required: true,
+        select: false,
+    }
+})
+
+module.exports = mongoose.model('ride', rideSchema);
